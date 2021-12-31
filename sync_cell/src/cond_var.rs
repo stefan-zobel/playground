9e4ceb9a9308c676185d02ccc3618f396259578f
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-12-31 01:02:38 +0100

    add a Condvar adapter for ReentrantMutexGuard (untested)

---
+++ sync_cell/src/cond_var.rs
@@ -0,0 +1,39 @@
+use parking_lot::lock_api::{GetThreadId, RawMutex, ReentrantMutexGuard};
+use parking_lot::{Condvar, Mutex};
+
+pub type RCondVar = CondVarForRMutexGuard;
+
+pub struct CondVarForRMutexGuard {
+    c: Condvar,
+    m: Mutex<()>,
+}
+
+impl CondVarForRMutexGuard {
+    #[inline]
+    pub fn new() -> CondVarForRMutexGuard {
+        CondVarForRMutexGuard {
+            c: Condvar::new(),
+            m: Mutex::new(()),
+        }
+    }
+
+    // see https://github.com/Amanieu/parking_lot/issues/165
+    #[inline]
+    pub fn wait<R: RawMutex, G: GetThreadId, T: ?Sized>(
+        &self,
+        g: &mut ReentrantMutexGuard<'_, R, G, T>,
+    ) {
+        let new_guard = self.m.lock();
+        ReentrantMutexGuard::unlocked(g, || {
+            // Move the new guard in so it gets unlocked
+            // before we re-lock the ReentrantMutexGuard
+            let mut guard = new_guard;
+            self.c.wait(&mut guard);
+        });
+    }
+
+    #[inline]
+    pub fn notify_all(&self) {
+        self.c.notify_all();
+    }
+}
--- sync_cell/src/lib.rs
+++ sync_cell/src/lib.rs
@@ -1,3 +1,5 @@
 
 pub mod sync_cell;
+pub mod cond_var;
+pub mod arc_sync_cell;
 pub mod test_pool;
