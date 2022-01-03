9bd3274ec2d930ca5524058275afc1659dc36e6b
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-01-04 00:40:18 +0100

    trying to get it right this time

--- rwcell/src/rw_cell.rs
+++ rwcell/src/rw_cell.rs
@@ -1,49 +1,40 @@
 use crate::lock::{Lock, ReadGuard, WriteGuard};
 use crate::rw_cell_error::RwCellError;
 use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
 
 #[derive(Debug)]
 pub struct RwCell<T: ?Sized> {
     lock: Lock<UnsafeCell<T>>,
 }
 
-//unsafe impl<T: ?Sized + Send> Send for Lock<T> {}
-//unsafe impl<T: ?Sized + Send + Sync> Sync for Lock<T> {}
-
-//unsafe impl<T: ?Sized + Send> Send for SyncCell<T> {}
-//unsafe impl<T: ?Sized> Sync for SyncCell<T> {}
+unsafe impl<T: ?Sized + Send> Send for RwCell<T> {}
+unsafe impl<T: ?Sized + Send> Sync for RwCell<T> {}
+//unsafe impl<T: ?Sized + Send + Sync> Sync for RwCell<T> {}
 
 impl<T> RwCell<T> {
     pub fn try_borrow(&self) -> Option<RwRef<'_, T>> {
-        self.lock.try_read_shared().map(|lock| RwRef {
-            cell: self,
-            guard: lock,
-        })
+        self.lock
+            .try_read_shared()
+            .map(|lock| RwRef { guard: lock })
     }
 
     pub fn try_borrow_mut(&self) -> Option<RwRefMut<'_, T>> {
-        self.lock.try_write_exclusive().map(|lock| RwRefMut {
-            cell: self,
-            guard: lock,
-        })
+        self.lock
+            .try_write_exclusive()
+            .map(|lock| RwRefMut { guard: lock })
     }
 
     pub fn borrow(&self) -> Result<RwRef<'_, T>, RwCellError> {
         match self.lock.read_shared() {
-            Ok(lock) => Ok(RwRef {
-                cell: self,
-                guard: lock,
-            }),
+            Ok(lock) => Ok(RwRef { guard: lock }),
             Err(_) => Err(RwCellError {}),
         }
     }
 
     pub fn borrow_mut(&self) -> Result<RwRefMut<'_, T>, RwCellError> {
         match self.lock.write_exclusive() {
-            Ok(lock) => Ok(RwRefMut {
-                cell: self,
-                guard: lock,
-            }),
+            Ok(lock) => Ok(RwRefMut { guard: lock }),
             Err(_) => Err(RwCellError {}),
         }
     }
@@ -51,12 +42,32 @@ impl<T> RwCell<T> {
 
 #[derive(Debug)]
 pub struct RwRef<'scope, T: ?Sized> {
-    cell: &'scope RwCell<T>,
     guard: ReadGuard<'scope, UnsafeCell<T>>,
+}
+
+impl<T> Deref for RwRef<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.guard.deref().get() }
+    }
 }
 
 #[derive(Debug)]
 pub struct RwRefMut<'scope, T: ?Sized> {
-    cell: &'scope RwCell<T>,
     guard: WriteGuard<'scope, UnsafeCell<T>>,
+}
+
+impl<T> Deref for RwRefMut<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.guard.deref().get() }
+    }
+}
+
+impl<T> DerefMut for RwRefMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.guard.deref().get() }
+    }
 }
