83d665356c8a986d512c2bc583b4844ac8bf8381
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-01-10 01:28:22 +0100

    add the two other impls to the perf test

--- rnd/src/main.rs
+++ rnd/src/main.rs
@@ -1,4 +1,4 @@
-use rnd::pseudo_random::{Lcg64Xor1024Mix, PseudoRandom};
+use rnd::pseudo_random::{Lcg64Xor1024Mix, PseudoRandom, Stc64, XoShiRo256StarStar};
 use std::time::Instant;
 
 fn main() {
@@ -11,6 +11,24 @@ fn main() {
     }
     let stop = Instant::now();
     let duration = stop.duration_since(start);
-    println!("took {} ms", duration.as_millis());
+    println!("Lcg64Xor1024Mix took {} ms", duration.as_millis());
+    println!("last rand: {}", rand);
+    let mut prng2 = XoShiRo256StarStar::new_from(seed);
+    let start = Instant::now();
+    for _ in 0..100_000_000 {
+        rand = prng2.next_double();
+    }
+    let stop = Instant::now();
+    let duration = stop.duration_since(start);
+    println!("XoShiRo256StarStar took {} ms", duration.as_millis());
+    println!("last rand: {}", rand);
+    let mut prng3 = Stc64::new_from(seed);
+    let start = Instant::now();
+    for _ in 0..100_000_000 {
+        rand = prng3.next_double();
+    }
+    let stop = Instant::now();
+    let duration = stop.duration_since(start);
+    println!("Stc64 took {} ms", duration.as_millis());
     println!("last rand: {}", rand);
 }
