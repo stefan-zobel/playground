3c790cdc645b32975809d31e33b97c0c8d024764
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-01-11 00:09:10 +0100

    simplify elapsed calculation

--- rnd/src/main.rs
+++ rnd/src/main.rs
@@ -9,26 +9,20 @@ fn main() {
     for _ in 0..100_000_000 {
         rand = prng.next_double();
     }
-    let stop = Instant::now();
-    let duration = stop.duration_since(start);
-    println!("Lcg64Xor1024Mix took {} ms", duration.as_millis());
+    println!("Lcg64Xor1024Mix took {} ms", start.elapsed().as_millis());
     println!("last rand: {}", rand);
     let mut prng2 = XoShiRo256StarStar::new_from(seed);
     let start = Instant::now();
     for _ in 0..100_000_000 {
         rand = prng2.next_double();
     }
-    let stop = Instant::now();
-    let duration = stop.duration_since(start);
-    println!("XoShiRo256StarStar took {} ms", duration.as_millis());
+    println!("XoShiRo256StarStar took {} ms", start.elapsed().as_millis());
     println!("last rand: {}", rand);
     let mut prng3 = Stc64::new_from(seed);
     let start = Instant::now();
     for _ in 0..100_000_000 {
         rand = prng3.next_double();
     }
-    let stop = Instant::now();
-    let duration = stop.duration_since(start);
-    println!("Stc64 took {} ms", duration.as_millis());
+    println!("Stc64 took {} ms", start.elapsed().as_millis());
     println!("last rand: {}", rand);
 }
