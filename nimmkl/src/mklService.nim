177cc5fbf44ab97cbdb36badd37870d7ac3986e5
Stefan Zobel <istec.zobel@extaccount.com>
2022-09-28 11:45:39 +0200

    drop the Nim experiment

--- nimmkl/src/mklService.nim
+++
@@ -1,7 +0,0 @@
-import mklTypes
-
-type
-  XerblaEntry* = proc (name: cstring; num: ptr cint; len: cint) {.cdecl.}
-
-proc mklSetXerbla*(xerbla: XerblaEntry): XerblaEntry {.cdecl,
-    importc: "mkl_set_xerbla", dynlib: libName.}
